7e5fed6ffd18e658733ecc6b5deedd65800212d4
SardorBoboyev <boboyevsardor350@gmail.com>
2024-09-27 11:02:51 +0500

    Initial commit

---
+++ src/main/java/uz/sb/storyservice/StoryServiceApplication.java
@@ -0,0 +1,13 @@
+package uz.sb.storyservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StoryServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StoryServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/uz/sb/storyservice/StoryServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package uz.sb.storyservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StoryServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
